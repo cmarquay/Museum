9af9fca5030f2ba98184802a220ba5e0af4fa2d2
Christian Marquay <christian.marquay@etudiant.univ-lr.fr>
2017-06-13 15:43:41 +0200

    Adding static pages for the map

--- AndroidApp/app/src/main/java/com/example/android/museum/FlayedActivity.java
+++ AndroidApp/app/src/main/java/com/example/android/museum/FlayedActivity.java
@@ -3,12 +3,12 @@ package com.example.android.museum;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MapActivity extends AppCompatActivity {
+public class FlayedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+        setContentView(R.layout.activity_flayed);
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.mipmap.logo);
